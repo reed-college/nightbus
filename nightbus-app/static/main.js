ef48ff99803ec2716f99cb082239a6b6e5d8d7a7
Margie Oxley <margieoxley@c027h208.wless.reed.edu>
2017-05-30 11:06:44 -0700

    fade out for status updated message

--- nightbus-app/static/main.js
+++ nightbus-app/static/main.js
@@ -1,25 +1,39 @@
 $(document).ready(function(){
     $('#coming').click(function(){
         $('.statusbox').append('<p>Status updated!</p>');
+        $('.statusbox').fadeOut(4000)
     })
+
     $(document).on('click','#coming',function() {
         $('p').remove()
+        $('.statusbox').show()
         $('.statusbox').append('<p>Status updated!</p>');
+        $('.statusbox').fadeOut(4000)
    })
         $('#here').click(function(){
+        $('p').remove()
+        $('.statusbox').show()
         $('.statusbox').append('<p>Status updated!</p>');
+        $('.statusbox').fadeOut(4000)
     })
     $(document).on('click','#here',function() {
         $('p').remove()
+        $('.statusbox').show()
         $('.statusbox').append('<p>Status updated!</p>');
+        $('.statusbox').fadeOut(4000)
    })
 
     $('#leaving').click(function(){
         $('.statusbox').append('<p>Status updated!</p>');
+        $('.statusbox').fadeOut(4000)
     })
     $(document).on('click','#leaving',function() {
         $('p').remove()
+        $('.statusbox').show()
         $('.statusbox').append('<p>Status updated!</p>');
+        $('.statusbox').fadeOut(4000)
    })
 })
+
+
 
