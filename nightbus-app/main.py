81bb34580e1f6bb02e9e4479dbc10c374db926b1
Abenezer Mamo <abmamo@reed.edu>
2017-06-06 17:48:38 -0700

    Still trying to bind to port

--- nightbus-app/main.py
+++ nightbus-app/main.py
@@ -262,5 +262,5 @@ def no_user():
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT'))
     app.run(host='0.0.0.0', port=port)
