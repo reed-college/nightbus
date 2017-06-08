b87b6cd868de76c557b8db0aa8f4dd434ecf6cb2
Abenezer Mamo <abmamo@reed.edu>
2017-06-08 14:13:31 -0700

    Changed which username the app uses to connect to the application

--- nightbus-app/database.py
+++ nightbus-app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy import (Column, Integer, String,
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-url = "postgres://pakqjossssuvfz:b6d2cf54195a2103c58a1026e3b8a5c3abe6461491e4b534ef8b29963219387e@ec2-50-17-236-15.compute-1.amazonaws.com:5432/pakqjossssuvfz"
+url = "postgres://pakqjossssuvfz:b6d2cf54195a2103c58a1026e3b8a5c3abe6461491e4b534ef8b29963219387e@ec2-50-17-236-15.compute-1.amazonaws.com:5432/dfaectjl5ak2mm"
 engine = sqlalchemy.create_engine(url)
 
 def get_session():
