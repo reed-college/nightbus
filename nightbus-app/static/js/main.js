e48009cd98598965bc3bb7d1378d05761d938091
Hien Nguyen <nguyehi@reed.edu>
2017-07-25 15:37:33 -0700

    fixed merge conflicts

--- nightbus-app/static/js/main.js
+++ nightbus-app/static/js/main.js
@@ -135,6 +135,11 @@ function validateMissing() {
 
     errorPlacement: function(error, element) {
       error.appendTo($('#missing-errors'));
+      $('#missing-errors').show();
+      //clear error messages whilst typing
+        element.keyup( function() {
+         $('#missing-errors').empty().hide();
+       });
       },
       rules: {
         name: "required",
@@ -176,6 +181,11 @@ function validateLate() {
 
     errorPlacement: function(error, element) {
       error.appendTo($('#late-errors'));
+      $('#late-errors').show();
+      //clear error messages whilst typing
+        element.keyup( function() {
+         $('#late-errors').empty().hide();
+       });
       },
       rules: {
         name: "required",
