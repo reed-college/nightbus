c7d8624ceb344f5cd68067eafa638e8a17b2e693
Hien Nguyen <nguyehi@reed.edu>
2017-07-31 15:26:16 -0700

    moved fb credentials to local config

--- nightbus-app/post_to_fb.py
+++ nightbus-app/post_to_fb.py
@@ -1,17 +1,18 @@
 
 import facebook
 import requests
+import local_config
 
 proxies = {
     "http":"http://tinyproxy.reed.edu:8888/",
     "https": "https://tinyproxy.reed.edu:8888/"
 }
+
 def main(message):
     cfg = {
-        "page_id": 132893597268288,
-        "access_token": "EAAUg264tb5wBADKZA3dn8x1TUaFWIGGG2ouCOfAfERxnOajfZBaplk2Ivnv2TZATNqd1oCMYNohhwoVzmhN9VZAykP3nfhxQ9xKhQXc2ncdr8qcYwpWrHHZBcQZB2WZBU4OiaBZCZAgHehUhKK3r1sdHybT3tN8E68loZD"
+        "page_id": local_config.FACEBOOK_PAGE_ID,
+        "access_token": local_config.FACEBOOK_ACCESS_TOKEN
     }
-
     api = get_api(cfg)
     msg = message
     status = api.put_wall_post(msg)
@@ -20,8 +21,8 @@ def get_api(cfg):
     graph = facebook.GraphAPI(cfg['access_token'])
     resp = graph.get_object('me/accounts')
     for page in resp['data']:
-         page_access_token = page['access_token']
-    graph = facebook.GraphAPI(page_access_token, proxies=proxies)
+        page_access_token = page['access_token']
+    graph = facebook.GraphAPI(access_token=page_access_token, proxies=proxies)
     return graph
 
 if __name__ == "__main__":
